3a91c3df01845cd98ee0bd33b141d711c99e93ab
Tibo Vanheule <tibo@tibovanheule.space>
2023-03-05 14:13:48 +0100

    frontend

--- main.py
+++ main.py
@@ -2,38 +2,47 @@
 import argparse
 import time
 import cv2
-import os
 import numpy as np
 import multiprocessing
+from PIL import Image, ImageFont, ImageDraw
 
 class booth():
 
-    def cvText(self,frame,text,loc,font,size):
-        cv2.putText(frame, text,loc,font,size,(255,255,255,255),12,cv2.LINE_AA)
-        cv2.putText(frame, text,loc,font,size,(0,0,0,0),4,cv2.LINE_AA)
+    def cvText(self,text,loc,font=None):
+        # Make into PIL Image
+        im_p = Image.fromarray(self.state['frame'])
+        # Get a drawing context
+        draw = ImageDraw.Draw(im_p)
+        if font is None:
+            draw.text(loc,text,(255,255,255),font=self.salt)
+        else:
+            draw.text(loc,text,(255,255,255),font=font)
+        # Convert back to OpenCV image and save
+        self.state['frame'] = np.array(im_p)
 
-    def save_frame(self,state,frame):
-        fname = os.path.join(state['path'],'booth_'+str(int(time.time()))+'.png')
+    def save_frame(self,frame):
+        fname =  str("booth/booth_"+str(int(time.time()))+'.png')
         cv2.imwrite(fname,frame)
-        state['worklist'].append(fname)
+        self.state['worklist'].append(fname)
 
 
-    def normal(self,state):
-        if state["Snap"]:
-            display_time = state['countdown'] - int(time.time() - state['start_time'] )
-            state['frame2'] = state['frame'].copy()
+    def normal(self):
+        if self.state["Snap"]:
+            display_time = self.state['countdown'] - int(time.time() - self.state['start_time'] )
+            self.state['frame2'] = self.state['frame'].copy()
             if display_time > -1:
-                self.cvText(state['frame'], str(display_time),(610,385),state['font'],3)
+                self.cvText(str(display_time),(610-20,385-150))
             if display_time < 1:
-                if not(state['Freeze']):
-                    state['Freeze'] = True
-                    state['Freeze_frame'] = state['frame2'].copy()
-                    self.save_frame(state,state['frame2'])
-                if state['Freeze']:
-                    state['frame'] = state['Freeze_frame'].copy()
+                if not(self.state['Freeze']):
+                    self.state['Freeze'] = True
+                    self.state['Freeze_frame'] = self.state['frame2'].copy()
+                    self.save_frame(self.state['frame2'])
+                if self.state['Freeze']:
+                    self.state['frame'] = self.state['Freeze_frame'].copy()
+                    self.cvText("http://homekonvent.be/photobooth",(210-20,585-150),font=self.salt_smallest)
             if display_time < -1:
-                state['Snap'] = False
-                state['Freeze'] = False
+                self.state['Snap'] = False
+                self.state['Freeze'] = False
 
     def quad_image(self,frame1,frame2,frame3,frame4):
         frame = np.zeros((720,1280,3),np.uint8)
@@ -47,10 +56,10 @@ class booth():
         frame[360:720,640:1280] = tmp_frame[0:360,0:640]
         return frame
 
-    def cartoon(self,state):
+    def cartoon(self):
         num_down = 2
         num_bilateral = 7
-        img_colour = state['frame']
+        img_colour = self.state['frame']
 
         for _ in range(num_down):
             img_colour = cv2.pyrDown(img_colour)
@@ -61,7 +70,7 @@ class booth():
         for _ in range(num_down):
             img_colour = cv2.pyrUp(img_colour)
 
-        img_grey = cv2.cvtColor(state['frame'],cv2.COLOR_RGB2GRAY)
+        img_grey = cv2.cvtColor(self.state['frame'],cv2.COLOR_RGB2GRAY)
         img_blur = cv2.medianBlur(img_grey, 7)
 
         img_edge = cv2.adaptiveThreshold(img_blur, 255,
@@ -70,12 +79,12 @@ class booth():
                                         blockSize=9,
                                         C=2)
         img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-        state['frame'] = cv2.bitwise_and(img_colour, img_edge)
-        if state['Snap']:
-            self.normal(state)
+        self.state['frame'] = cv2.bitwise_and(img_colour, img_edge)
+        if self.state['Snap']:
+            self.normal()
 
-    def four_col(self,state):
-        tmp_frame = cv2.resize(state['frame'],None,fx=0.5,fy=0.5)
+    def four_col(self):
+        tmp_frame = cv2.resize(self.state['frame'],None,fx=0.5,fy=0.5)
         tmp_grey = cv2.cvtColor(tmp_frame, cv2.COLOR_RGB2GRAY)
         tmp_invert = cv2.bitwise_not(tmp_grey)
         tmp_zeros = np.zeros((360,640),np.uint8)
@@ -101,88 +110,91 @@ class booth():
         tmp_frame4[0:360,0:640,0] = tmp_grey
         tmp_frame4[0:360,0:640,1] = tmp_grey
 
-        state['frame'][0:360,0:640] = tmp_frame1[0:360,0:640]
-        state['frame'][0:360,640:1280] = tmp_frame2[0:360,0:640]
-        state['frame'][360:720,0:640] = tmp_frame3[0:360,0:640]
-        state['frame'][360:720,640:1280] = tmp_frame4[0:360,0:640]
+        self.state['frame'][0:360,0:640] = tmp_frame1[0:360,0:640]
+        self.state['frame'][0:360,640:1280] = tmp_frame2[0:360,0:640]
+        self.state['frame'][360:720,0:640] = tmp_frame3[0:360,0:640]
+        self.state['frame'][360:720,640:1280] = tmp_frame4[0:360,0:640]
 
-        if state['Snap']:
-            self.normal(state)
+        if self.state['Snap']:
+            self.normal()
 
-    def sepia(self,state):
-        frame = state['frame'].copy()
+    def sepia(self):
+        frame = self.state['frame'].copy()
         m_sepia = np.asarray([[0.272, 0.534, 0.131],
                                 [0.349, 0.686, 0.168],
                                 [0.393, 0.769, 0.189]])
-        state['frame'] = cv2.transform(frame, m_sepia)
-        if state['Snap']:
-            self.normal(state)
+        self.state['frame'] = cv2.transform(frame, m_sepia)
+        if self.state['Snap']:
+            self.normal()
 
-    def grey(self,state):
-        img_grey = cv2.cvtColor(state['frame'],cv2.COLOR_RGB2GRAY)
-        state['frame'] = cv2.cvtColor(img_grey,cv2.COLOR_GRAY2RGB)
-        if state['Snap']:
-            self.normal(state)
-
-    def erode(self,state):
-        kernel = np.ones((5,5),np.uint8)
-        state['frame'] = cv2.erode(state['frame'],kernel,iterations =3)
-        if state['Snap']:
-            self.normal(state)
-
-
-
+    def grey(self):
+        img_grey = cv2.cvtColor(self.state['frame'],cv2.COLOR_RGB2GRAY)
+        self.state['frame'] = cv2.cvtColor(img_grey,cv2.COLOR_GRAY2RGB)
+        if self.state['Snap']:
+            self.normal()
 
     def __init__(self, id) -> None:
-        state = {'Snap': False,
+        self.state = {'Snap': False,
                 "Freeze": False,
                 "countdown": 5,
                 "mode": 0,
-                'font': cv2.FONT_HERSHEY_SIMPLEX,
                 'frame_no': 1,
                 'path':"."}
         #load_config(state)
         m = multiprocessing.Manager()
-        worklist = m.list()
-        p = multiprocessing.Process(target = other_process,args=(worklist,))
+        self.worklist = m.list()
+        p = multiprocessing.Process(target = other_process,args=(self.worklist,))
         p.start()
-        state['worklist'] = worklist
-        filters = [self.normal,self.cartoon,self.four_col,self.sepia,self.grey,self.erode]
+        self.state['worklist'] = self.worklist
+        filters = [self.normal,self.cartoon,self.four_col,self.sepia,self.grey]
         cap = cv2.VideoCapture(id)
         cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
         ret = 1
+        self.salt = ImageFont.truetype("salt.ttf",150)
+        self.salt_smallest = ImageFont.truetype("salt.ttf",50)
+        salt = ImageFont.truetype("salt.ttf",100)
         while ret:
-            ret, state['frame'] = cap.read()
-            filters[state['mode']](state)
+            ret, self.state['frame'] = cap.read()
+            filters[self.state['mode']]()
             cv2.namedWindow("PhotoBooth", cv2.WND_PROP_FULLSCREEN)
             cv2.setWindowProperty("PhotoBooth", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-            cv2.imshow('PhotoBooth',state['frame'])
+            if not(self.state['Snap']):
+                # Make into PIL Image
+                im_p = Image.fromarray(self.state['frame'])
+                # Get a drawing context
+                draw = ImageDraw.Draw(im_p)
+                
+                draw.text((610-200,385-100),"Take a picture",(255,255,255),font=salt)
+                with Image.open("arrow.png") as im:
+                    im.thumbnail((128*2,128*2), Image.Resampling.LANCZOS)
+                    im_p.paste(im,(150,300),mask=im)
+                # Convert back to OpenCV image and save
+                self.state['frame'] = np.array(im_p)
+            cv2.imshow('PhotoBooth',self.state['frame'])
             key = cv2.waitKey(1) & 0xFF
             if key == 27:
                 break
-            if not(state['Snap']):
+            if not(self.state['Snap']):
                 if key == ord(' '):
-                    state['Snap'] = True
-                    state['start_time'] = time.time()
+                    self.state['Snap'] = True
+                    self.state['start_time'] = time.time()
                 if key == ord('1'):
-                    state['mode'] = 0
+                    self.state['mode'] = 0
                 if key == ord('2'):
-                    state['mode'] = 1
+                    self.state['mode'] = 1
                 if key == ord('3'):
-                    state['mode'] = 2
+                    self.state['mode'] = 2
                 if key == ord('4'):
-                    state['mode'] = 3
+                    self.state['mode'] = 3
                 if key == ord('5'):
-                    state['mode'] = 4
-                if key == ord('6'):
-                    state['mode'] = 5
+                    self.state['mode'] = 4
         cap.release()
         cv2.destroyAllWindows()
-        while len(worklist) > 0:
+        while len(self.worklist) > 0:
             print('Sleeping main')
             time.sleep(1)
-        worklist.append("QUIT")
+        self.worklist.append("QUIT")
         p.join()
 
 def returnCameraIndexes():
@@ -210,7 +222,6 @@ def other_process(worklist):
             print ('Uploading:',(fname))
             #UPLOAD
         else:
-            print('sleeping')
             time.sleep(1)
             sleep_timer += 1
             #if sleep_timer > 10000:
@@ -218,7 +229,7 @@ def other_process(worklist):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-                        prog = 'Pothobooth',
+                        prog = 'Photobooth',
                         description = 'photobooth',
                         epilog = ' give camera index using -c flag')
 
