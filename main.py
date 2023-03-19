c6cccf66a83ccad22ac2588fcf919db99f4a43cd
Tibo Vanheule <tibo@tibovanheule.space>
2023-03-19 10:22:45 +0100

    mac os push

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from pathlib import Path
 import argparse
 import base64
 import time
@@ -25,22 +25,24 @@ class booth():
     def save_frame(self,frame):
         fname =  str("booth/booth_"+str(int(time.time()))+'.png')
         cv2.imwrite(fname,frame)
-        self.state['worklist'].append(fname)
+        jpg_img = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+        b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
+        requests.post("https://homekonvent.be/photobooth/image",json={"image":b64_string})
 
 
     def normal(self):
         if self.state["Snap"]:
             display_time = self.state['countdown'] - int(time.time() - self.state['start_time'] )
-            self.state['frame2'] = self.state['frame'].copy()
+            self.state['frame2'] = self.state['frame']
             if display_time > -1:
                 self.cvText(str(display_time),(610-20,385-150))
             if display_time < 1:
                 if not(self.state['Freeze']):
                     self.state['Freeze'] = True
-                    self.state['Freeze_frame'] = self.state['frame2'].copy()
+                    self.state['Freeze_frame'] = self.state['frame2']
                     self.save_frame(self.state['frame2'])
                 if self.state['Freeze']:
-                    self.state['frame'] = self.state['Freeze_frame'].copy()
+                    self.state['frame'] = self.state['Freeze_frame']
                     self.cvText("http://homekonvent.be/photobooth",(210-20,585-150),font=self.salt_smallest)
             if display_time < -1:
                 self.state['Snap'] = False
@@ -138,20 +140,14 @@ class booth():
     def __init__(self, id) -> None:
         self.state = {'Snap': False,
                 "Freeze": False,
-                "countdown": 5,
+                "countdown": 10,
                 "mode": 0,
-                'frame_no': 1,
+                'frame_no': 5,
                 'path':"."}
-        #load_config(state)
-        m = multiprocessing.Manager()
-        self.worklist = m.list()
-        p = multiprocessing.Process(target = other_process,args=(self.worklist,))
-        p.start()
-        self.state['worklist'] = self.worklist
         filters = [self.normal,self.cartoon,self.four_col,self.sepia,self.grey]
         cap = cv2.VideoCapture(id)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
         ret = 1
         self.salt = ImageFont.truetype("salt.ttf",150)
         self.salt_smallest = ImageFont.truetype("salt.ttf",50)
@@ -162,25 +158,13 @@ class booth():
             cv2.namedWindow("PhotoBooth", cv2.WND_PROP_FULLSCREEN)
             cv2.setWindowProperty("PhotoBooth", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
             if not(self.state['Snap']):
-                # Make into PIL Image
-                im_p = Image.fromarray(self.state['frame'])
-                # Get a drawing context
-                draw = ImageDraw.Draw(im_p)
-                
-                draw.text((610-200,385-100),"Take a picture",(255,255,255),font=salt)
-                with Image.open("arrow.png") as im:
-                    im.thumbnail((128*2,128*2), Image.Resampling.LANCZOS)
-                    im_p.paste(im,(150,300),mask=im)
-                # Convert back to OpenCV image and save
-                self.state['frame'] = np.array(im_p)
+                self.state['Snap'] = True
+                self.state['start_time'] = time.time()
             cv2.imshow('PhotoBooth',self.state['frame'])
             key = cv2.waitKey(1) & 0xFF
             if key == 27:
                 break
             if not(self.state['Snap']):
-                if key == ord(' '):
-                    self.state['Snap'] = True
-                    self.state['start_time'] = time.time()
                 if key == ord('1'):
                     self.state['mode'] = 0
                 if key == ord('2'):
@@ -193,11 +177,6 @@ class booth():
                     self.state['mode'] = 4
         cap.release()
         cv2.destroyAllWindows()
-        while len(self.worklist) > 0:
-            print('Sleeping main')
-            time.sleep(1)
-        self.worklist.append("QUIT")
-        p.join()
 
 def returnCameraIndexes():
     # checks the first 10 indexes.
@@ -216,20 +195,6 @@ def returnCameraIndexes():
             return arr
     return arr
 
-def other_process(worklist):
-
-    while 'QUIT' not in worklist:
-        if len(worklist) > 0:
-            fname = worklist.pop()
-            print ('Uploading:',(fname))
-            #UPLOAD
-            img = cv2.imread(fname)
-            jpg_img = cv2.imencode('.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
-            b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
-            requests.post("https://homekonvent.be/photobooth/image",json={"image":b64_string})
-        else:
-            time.sleep(1)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                         prog = 'Photobooth',
@@ -243,3 +208,4 @@ if __name__ == '__main__':
         booth(int(args.camera))
     else:
         print(returnCameraIndexes())
+
